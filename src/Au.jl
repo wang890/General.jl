055545f2e64cf06dc7b8efc9d974912b1b747abc
wang890 <wisecash@126.com>
2024-04-21 00:03:16 +0800

    if Main.log

--- src/Au.jl
+++ src/Au.jl
@@ -14,33 +14,31 @@ module Aux
         # not_show_my_debug = false  # 否定之否定 为肯定，即显示        
         # if not_show_my_debug
         # if ! Aux.log        
-        if ! Main.log
-            return nothing
-        end
-              
-        exs = collect(exs)
-        exs[1] = exs[1] isa String ? exs[1] : string(exs[1])         
+        # if ! Main.log
+        #     return nothing
+        # end
+        if Main.log  
+            exs = collect(exs)
+            exs[1] = exs[1] isa String ? exs[1] : string(exs[1])         
 
-        if exs[end] in Layout.level__hr_keys
-            hr_level = pop!(exs)
-        else
-            hr_level = 1
-        end
+            if exs[end] in Layout.level__hr_keys
+                hr_level = pop!(exs)
+            else
+                hr_level = 1
+            end            
+            # global level__hr
+            hr = Layout.level__hr[hr_level]*exs[1]
         
-        # global level__hr
-        hr = Layout.level__hr[hr_level]*exs[1]
-    
-        replacing = "\n      "
-        replaced = replace(hr, '\n'=> replacing)
-        if hr_level == "3" && length(exs[1]) == 0 
-            replaced = replaced[1:end-7]
-        end
-        exs[1] =  replaced    
+            replacing = "\n      "
+            replaced = replace(hr, '\n'=> replacing)
+            if hr_level == "3" && length(exs[1]) == 0 
+                replaced = replaced[1:end-7]
+            end
+            exs[1] =  replaced    
 
-        Base.CoreLogging.logmsg_code(
-            (Base.CoreLogging.@_sourceinfo)..., 
-            esc(Base.CoreLogging.Info), exs...
-            )
+            Base.CoreLogging.logmsg_code((Base.CoreLogging.@_sourceinfo)..., esc(Base.CoreLogging.Info), exs...)
+        end        
+        
     end
 
     export @log
